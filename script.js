2712da4df5df2a74045817094ffe4a03cae643a6
rahul2894 <139780003+rahul2894@users.noreply.github.com>
2025-02-09 01:00:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const secretCode = new Set(['divu', 'sonu', 'divyani']); // Replace with your secret code
-const nextPageUrl = 'main.html'; // Replace with the URL of the new page
-
 const form = document.getElementById('login-form');
 const codeInput = document.getElementById('code');
 const submitBtn = document.getElementById('submit-btn');
 const resultDiv = document.getElementById('result');
 
+const secretCode = 'Divu'; // Replace with your secret code
+const nextPageUrl = 'main.html'; // Replace with the URL of the new page
+
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const userInput = codeInput.value.trim();
-	if (secretCodes.has(userInput) {
+	if (userInput === secretCode) {
 		resultDiv.innerHTML = 'Baby';
 		window.location.href = nextPageUrl; // Redirect to new page
 	} else {
